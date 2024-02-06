ec1ac8e8baf8d6904e1fe9706b318fc58e90e1b7
Marcílio Bezerra <marcilio.bc@gmail.com>
2024-02-06 08:03:24 -0300

    Criando Classes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+using System.Xml;
+
+Musica musica1 = new Musica();
+musica1.nome = "Roxanne";
+musica1.artista = "The Police";
+musica1.duracao = 5;
+musica1.disponivel = true;
+Console.WriteLine(musica1.nome);
+
+
